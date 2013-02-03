2b56fc4fdf2bdc5c1b0580b28f03f51235012f8a
Joel Black <joel.black@mobettertech.com>
2013-02-02 20:08:01 -0600

    added PantryManager Impl to server project.

---
+++ GRUB Core Domain/src/com/mobettertech/grub/core/shopping/IShoppingListManager.java
@@ -0,0 +1,23 @@
+/**
+ * IShoppingManager.java
+ *
+ * Copyright (c) 2013 Mo Better Tech, Co.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Mo Better Tech, Co.
+ * 
+ */
+package com.mobettertech.grub.core.shopping;
+
+/**
+ * This class implements the interface that defines the application 
+ * programming interface of the grocery shopping list manager.
+ *
+ * @author Joel Black
+ *
+ */
+public interface IShoppingListManager
+{
+
+}
---
+++ GRUB Server/src/com/mobettertech/grub/server/pantry/PantryManager.java
@@ -0,0 +1,71 @@
+/**
+ * PantryManager.java
+ *
+ * Copyright (c) 2013 Mo Better Tech, Co.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Mo Better Tech, Co.
+ * 
+ */
+package com.mobettertech.grub.server.pantry;
+
+import java.util.List;
+
+import com.mobettertech.grub.core.catalog.GroceryItem;
+import com.mobettertech.grub.core.pantry.IPantryManager;
+import com.mobettertech.grub.core.pantry.PantryException;
+
+
+/**
+ * This class implements
+ *
+ * @author Joel Black
+ *
+ */
+public class PantryManager implements IPantryManager
+{
+
+    @Override
+    public void addItem(GroceryItem aArg0) throws PantryException
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void markDepleted() throws PantryException
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public List<GroceryItem> reitrieveItemList() throws PantryException
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void removeItem(GroceryItem aArg0) throws PantryException
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public GroceryItem retrieveItem() throws PantryException
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void updateItem() throws PantryException
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+}
