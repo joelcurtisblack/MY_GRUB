04e982de7061ed85f04ff53cc008732637ee5f16
Joel Black <joel.black@mobettertech.com>
2013-12-30 15:42:43 -0600

    Created new Jdbc DAO implementation class

---
+++ GRUB Core Domain/src/com/mobettertech/grub/core/catalog/dao/jdbc/GroceryCatalogJdbcDao.java
@@ -0,0 +1,69 @@
+package com.mobettertech.grub.core.catalog.dao.jdbc;
+
+import java.util.Collection;
+
+import com.mobettertech.grub.core.catalog.ItemFilter;
+import com.mobettertech.grub.core.catalog.dao.GroceryCatalogDataAccessException;
+import com.mobettertech.grub.core.catalog.dao.IGroceryCatalogDao;
+import com.mobettertech.grub.core.catalog.dto.GroceryItem;
+
+public class GroceryCatalogJdbcDao implements  IGroceryCatalogDao
+{
+
+	@Override
+	public void createItem(GroceryItem aGroceryItem) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createItems(Collection<GroceryItem> aGroceryItemCollection) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<GroceryItem> retrieveAllItems() throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroceryItem retrieveItems(ItemFilter aItemFilter) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateItem(GroceryItem aGroceryItem) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateItems(Collection<GroceryItem> aGroceryItemList) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteItem(GroceryItem aGroceryItem) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteItems(Collection<GroceryItem> aGroceryItemList) throws GroceryCatalogDataAccessException 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
